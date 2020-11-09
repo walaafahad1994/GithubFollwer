d3b082694afd93868471fe9ce3532d404ac630b2
walaa <mac.lote@hotmail.com>
2020-11-09 17:56:18 +0300

    Initial Commit

---
+++ GithubFollwer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollwer
+//
+//  Created by walaa on 09/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubFollwerTests/GithubFollwerTests.swift
@@ -0,0 +1,33 @@
+//
+//  GithubFollwerTests.swift
+//  GithubFollwerTests
+//
+//  Created by walaa on 09/11/2020.
+//
+
+import XCTest
+@testable import GithubFollwer
+
+class GithubFollwerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
