bfa2f8217edc6fe8b0386d79637371d3c15a3cef
walaa <mac.lote@hotmail.com>
2020-11-24 10:15:33 +0300

    git commit -am "add photos"

--- GithubFollwer/SceneDelegate.swift
+++ GithubFollwer/SceneDelegate.swift
@@ -13,12 +13,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windoScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windoScene.coordinateSpace.bounds)
+        window?.windowScene = windoScene
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
     }
-
+    
+    func  createsearchVC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+    return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func  createfavioritesVC() -> UINavigationController {
+        let faviortiesVC = FaviortiesVC()
+        faviortiesVC.title = "Faviortes"
+        faviortiesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+    return UINavigationController(rootViewController: faviortiesVC)
+    }
+    func createTabBar() -> UITabBarController{
+        
+    let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabBar.viewControllers = [createsearchVC() , createfavioritesVC()]
+    return tabBar
+    }
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
---
+++ GithubFollwer/faviortiesVC.swift
@@ -0,0 +1,30 @@
+//
+//  faviortiesVC.swift
+//  GithubFollwer
+//
+//  Created by walaa on 09/11/2020.
+//
+
+import UIKit
+
+class FaviortiesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
